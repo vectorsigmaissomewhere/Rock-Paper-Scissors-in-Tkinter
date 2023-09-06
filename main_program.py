42928041d369951c618ef43013ce02f84011a77b
whereyoucan'tfind <74447376+vectorsigma001@users.noreply.github.com>
2023-09-06 07:07:16 -0700

    Update main_program.py

--- main_program.py
+++ main_program.py
@@ -1,8 +1,117 @@
 import tkinter as tk
+import pygame
+import random
+
+# Initialize pygame mixer and load a music file (for demo purposes, I'll use the same 'cara.mp3')
+pygame.mixer.init()
+pygame.mixer.music.load('cara.mp3')
+pygame.mixer.music.play(-1)  # Play the music in a loop
+
+def set_volume(val):
+    volume = float(val) / 100  # Scale's values are strings; convert to float and normalize
+    pygame.mixer.music.set_volume(volume)
+
+def on_closing():
+    pygame.mixer.music.stop()
+    window.destroy()
 
 window = tk.Tk()
 window.geometry("800x550")
 window.configure(bg="#FFFFFF")
+
 welcome_label = tk.Label(window, text="Home")
 welcome_label.place(x=133, y=25)
+
+volume_label = tk.Label(window, text="Volume", bg="#FFFFFF")
+volume_label.place(x=20, y=20)
+
+volume_scale = tk.Scale(window, from_=0, to=100, orient=tk.HORIZONTAL, command=set_volume)
+volume_scale.set(50)  # Default volume level: 50%
+volume_scale.place(x=20, y=50)
+
+firstplayerframe = tk.Frame(
+    window,
+    bg="#C522D4",
+    height=150,
+    width=200
+)
+firstplayerframe.place(x=300, y=50)
+
+secondplayerframe = tk.Frame(
+    window,
+    bg="#C522D4",
+    height=150,
+    width=200
+)
+secondplayerframe.place(x=50, y=300)
+
+yourframe = tk.Frame(
+    window,
+    bg="#C522D4",
+    height=150,
+    width=200
+)
+yourframe.place(x=500, y=300)
+
+rockbutton = tk.Button(yourframe, text="Rock", fg="black", bg="white", command="youchoserock")
+rockbutton.place(x=20, y=90)
+
+paperbutton = tk.Button(yourframe, text="Paper", fg="black", bg="white", command="youchosepaper")
+paperbutton.place(x=75, y=90)
+
+scissorsbutton = tk.Button(yourframe, text="Scissors", fg="black", bg="white", command="youchosescissors")
+scissorsbutton.place(x=135, y=90)
+
+chosen = ""
+result_label = tk.Label(
+    window,
+    text="",
+    font=("Trebuchet Ms", 15, "bold"),
+    fg="#000000",
+    bg="#FFFFFF",
+)
+result_label.place(x=350, y=450)
+
+def youchoserock():
+    global chosen
+    chosen = "rock"
+
+def youchosepaper():
+    global chosen
+    chosen = "paper"
+
+def youchosescissors():
+    global chosen
+    chosen = "scissors"
+
+def random_choice():
+    randomchosen = ["rock", "paper", "scissors"]
+    return random.choice(randomchosen)
+
+def determine_winner(player_choice, opponent_choice):
+    if player_choice == opponent_choice:
+        return "It's a tie!"
+    elif (
+        (player_choice == "rock" and opponent_choice == "scissors") or
+        (player_choice == "paper" and opponent_choice == "rock") or
+        (player_choice == "scissors" and opponent_choice == "paper")
+    ):
+        return "You win!"
+    else:
+        return "Opponent wins!"
+
+def play_game():
+    global chosen
+    if chosen:
+        opponent_choice = random_choice()
+        result = determine_winner(chosen, opponent_choice)
+        result_label.config(text=result)
+    else:
+        result_label.config(text="Choose a move!")
+
+play_button = tk.Button(yourframe, text="Play", fg="black", bg="white", command=play_game)
+play_button.place(x=75, y=120)
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+window.resizable(False, False)
 window.mainloop()
